bc282a60adc62d944787c433e9a1caf4591e575c
Anthony Albertorio <anthony.albertorio@gmail.com>
2017-04-05 18:25:47 -0400

    added '/blog' endpoin to app.js to serve mock json data from /mock/posts.json.

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 'use strict';
 
-var express = require('express');
+var express = require('express'),
+	  posts = require('./mock/posts.json');
 
 // copies express to app so that we can extend and manipulate it
 var app = express();
@@ -8,6 +9,10 @@ var app = express();
 // '/' refers to the root directory aka home page
 app.get('/', function(request, response){
 	response.send('<h1>I love Treehouse!</h1>');
+});
+
+app.get('/blog', function(request, response){
+	response.send(posts);
 });
 
 // get app to listen to port 3000
